60fa1d4e3153b6197650663befe1dfc171fb3714
Emil Palm <7381434+c11epm@users.noreply.github.com>
2022-12-12 23:28:31 +0100

    Did not clear this day sadge

--- dec12/main.c
+++ dec12/main.c
@@ -4,7 +4,7 @@
 
 #define WIDTH 64
 #define ROWS 41
-#define PATH 3
+#define PATH 8
 #define TRIED 2
 
 typedef struct coordinate {
@@ -16,13 +16,13 @@ int char_to_height(char a) {
     return ((int) (a)) - 96;
 }
 
-void dijkstra(int heights[ROWS][WIDTH], coordinate start) {
-
-}
-
 int is_walkable(int from, int to) {
     //Is allowed to move to place where value is one higher! And go to lower!
-    return (from - to) >= -1;
+    return (to - from) <= 1;
+}
+
+int is_visited(int **path, int x, int y) {
+    return path[y][x];
 }
 
 void print_matrix(int matrix[ROWS][WIDTH]) {
@@ -34,39 +34,62 @@ void print_matrix(int matrix[ROWS][WIDTH]) {
     }
 }
 
-int is_valid_position(int x, int y, int path[ROWS][WIDTH]) {
-    return x >= 0 && x < WIDTH && y >= 0 && y < ROWS && path[y][x] != TRIED;
+void print_matrix_pointer(int **matrix) {
+    for (int r = 0; r < ROWS; r++) {
+        for (int c = 0; c < WIDTH; c++) {
+            printf("%2d ", matrix[r][c]);
+        }
+        printf("\n");
+    }
+}
+
+int is_valid_position(int x, int y, int **path) {
+    if(x < 0 || x >= WIDTH) {
+        printf("X out of bounds\n");
+        return 0;
+    }
+    if(y < 0 || y >= ROWS) {
+        printf("y out of bounds\n");
+        return 0;
+    }
+    if(path[y][x] == TRIED) {
+        printf("point already visited\n");
+        return 0;
+    }
+    return  1;
 }
 
 //Check if next position is possible to move to
-int traverse(int x, int y, coordinate end, int map[ROWS][WIDTH], int *path[ROWS][WIDTH]) {
-    if (!is_valid_position(x, y)) {
+//TODO Maybe some sort of no_of_steps_param?
+int traverse(int x, int y, coordinate *end, int map[ROWS][WIDTH], int **path) {
+    if (!is_valid_position(x, y, path)) {
         return 0;
     }
-    if (x == end.x && y == end.y) {
-        printf("Goal!");
+    if (x == end->x && y == end->y) {
+        printf("Goal!\n");
         path[y][x] = PATH;
+        return 1;
     } else {
         path[y][x] = TRIED;
     }
 
     //North
-    if(traverse(x, y - 1, end, map, path)) {
+    if (is_walkable(map[y][x], map[y - 1][x]) && traverse(x, y - 1, end, map, path)) {
         path[y - 1][x] = PATH;
         return 1;
     }
     //West
-    if(traverse(x - 1, y, end, map, path)) {
+    if (is_walkable(map[y][x], map[y][x - 1]) && traverse(x - 1, y, end, map, path)) {
         path[y][x - 1] = PATH;
         return 1;
     }
     //South
-    if(traverse(x, y + 1, end, map, path)) {
+    if (is_walkable(map[y][x], map[y + 1][x]) && traverse(x, y + 1, end, map, path)) {
         path[y + 1][x] = PATH;
         return 1;
     }
     //East
-    if(traverse(x + 1, y, end, map, path)) {
+    if (is_walkable(map[y][x], map[y][x + 1]) && traverse(x + 1, y, end, map, path)) {
         path[y][x + 1] = PATH;
         return 1;
     }
@@ -89,11 +112,13 @@ int main(int argc, char **argv) {
         return -1;
     }
     int height_matrix[ROWS][WIDTH];
+    int **path = malloc(sizeof(int *) * ROWS);
     coordinate start;
     coordinate end;
     int row = 0;
     char line[64];
     while (fscanf(file, "%s\n", line) != -1) {
+        path[row] = malloc(sizeof(int) * WIDTH);
         for (int i = 0; i < strlen(line); i++) {
             if (line[i] == 'S') {
                 start.x = i;
@@ -110,9 +135,14 @@ int main(int argc, char **argv) {
         }
 
         row++;
-        printf("%s\n", line);
+        //printf("%s\n", line);
     }
 
+    traverse(start.x, start.y, &end, height_matrix, path);
+    //BFS(start.x, start.y, &end, height_matrix, path);
+    print_matrix_pointer(path);
+
+    printf("\n\n");
     print_matrix(height_matrix);
 
     fclose(file);

0f180ae11925136382b87d203cb202a14f11dba6
Emil Palm <7381434+c11epm@users.noreply.github.com>
2023-12-07 12:43:22 +0100

    solved day 2 problem 2

--- 2023/dec02/main.py
+++ 2023/dec02/main.py
@@ -9,6 +9,7 @@ rules["blue"] = 14
 
 impossible_games = []
 rows = len(file.split('\n'))
+power_sum = 0
 
 for i, line in enumerate(file.split('\n')):
     found_impossible = False
@@ -16,13 +17,20 @@ for i, line in enumerate(file.split('\n')):
     sets = sets.strip()
     t = sets.split(";")
     turns = [j.strip() for j in t]
+    power = {}
     for turn in turns:
         cubes = [k.strip() for k in turn.split(",")]
         for cube in cubes:
             number, cube_name = cube.split(" ")
+            if cube_name in power:
+                power[cube_name] = max(power[cube_name], int(number))
+            else:
+                power[cube_name] = int(number)
+
             if not found_impossible and rules[cube_name] < int(number):
                 found_impossible = True
                 impossible_games.append(i + 1)
+    power_sum += power["red"] * power["green"] * power["blue"]
 
-
-print(sum(list(i + 1 for i in range(rows)))- sum(impossible_games))
+print(sum(list(i + 1 for i in range(rows))) - sum(impossible_games))
+print(power_sum)

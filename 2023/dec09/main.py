1a6399466193536b1a38ead5a37014dfd984cfff
Emil Palm <7381434+c11epm@users.noreply.github.com>
2023-12-11 12:54:26 +0100

    solved day 9 p2

--- 2023/dec09/main.py
+++ 2023/dec09/main.py
@@ -14,10 +14,9 @@ def calc_different_list(input):
         difference.append(input[elem + 1] - input[elem])
     print(difference)
     if only_contains_zeroes(difference):
-        return input[-1]
+        return input[0]
     else:
-        return input[-1] + calc_different_list(difference)
-    # return difference
+        return input[0] - calc_different_list(difference)
 
 
 sum = 0;
